fe2f57ee88c35ee100c98e07011f36a0ac0685ed
Pauli Ojala <pauli@lacquer.fi>
2021-12-22 17:23:59 -0500

    Dominant and screenshare buttons + comp

--- js/src/react/hooks/mediaInput.js
+++ js/src/react/hooks/mediaInput.js
@@ -4,3 +4,32 @@ import { MediaInputContext } from '../contexts';
 export function useMediaInput() {
   return React.useContext(MediaInputContext);
 }
+
+export function useActiveVideo() {
+  const { activeVideoInputSlots } = React.useContext(MediaInputContext);
+
+  let activeIds = [];
+  let activeScreenshareIds = [];
+  let dominantId = null;
+  let maxSimultaneousVideoInputs = activeVideoInputSlots.length;
+
+  for (let i = 0; i < maxSimultaneousVideoInputs; i++) {
+    if (activeVideoInputSlots[i]) {
+      const videoId = i; // video ids are just indexes to the 'slots' array
+      activeIds.push(videoId);
+      if (activeVideoInputSlots[i].type === 'screenshare') {
+        activeScreenshareIds.push(videoId);
+      }
+      if (dominantId === null && activeVideoInputSlots[i].dominant) {
+        dominantId = videoId;
+      }
+    }
+  }
+
+  return {
+    activeIds,
+    activeScreenshareIds,
+    dominantId,
+    maxSimultaneousVideoInputs,
+  };
+}
