ee85d00addf6907347e6f66ced8590184011f817
Pauli Ojala <pauli@lacquer.fi>
2022-05-31 16:14:15 -0400

    VCS server-side image handling fixes

--- js/src/react/components/Image.js
+++ js/src/react/components/Image.js
@@ -1,13 +1,20 @@
 import * as React from 'react';
 
 export function Image(props) {
+  let { src } = props;
+
+  if (typeof src === 'string' && src.indexOf('.') === -1) {
+    // add default file extension
+    src += '.png';
+  }
+
   // can't use JSX in VCS core because it needs to run on Node without transpiling
   return React.createElement('image', {
     id: props.id,
     layout: props.layout,
     style: props.style || {},
     transform: props.transform || {},
-    src: props.src,
+    src,
     scaleMode: props.scaleMode || 'fit',
     blend: props.blend || {},
   });
