40d6fb420b6cc88d0bf324dad03aa97f9ac26f31
Pauli Ojala <pauli@lacquer.fi>
2023-03-03 16:44:07 +0200

    VCS module build command

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -33,84 +33,21 @@ const breakOnWarningPlugin = function () {
 // the user is expected to provide a composition id on the command line.
 // using moduleReplacementPlugin, the root import in vcs-browser.js is replaced
 // so that only the specified composition is loaded.
-let compositionImportPath;
+let g_compositionImportPath;
+let g_compId;
 
 const moduleReplacementPlugin = new webpack.NormalModuleReplacementPlugin(
   /(.*)__VCS_COMP_PATH__(\.*)/,
   function (resource) {
     resource.request = resource.request.replace(
       /__VCS_COMP_PATH__/,
-      compositionImportPath
+      g_compositionImportPath
     );
   }
 );
 
-export default function wwwClientConfig(env) {
-  const compId = env.compid;
-  if (!compId || compId.length < 1) {
-    console.error(
-      '** Must provide VCS composition id (use webpack CLI arg --env compid={id})'
-    );
-    process.exit(2);
-  }
-  if (!(compositionImportPath = getCompPathFromId(compId, 'browser'))) {
-    process.exit(3);
-  }
-
-  const isCompBundleDir =
-    path.basename(compositionImportPath).toLowerCase().indexOf('index.js') ===
-    0;
-
-  const dirsToCopy = [
-    { from: './devrig/example-assets', to: 'example-assets' },
-    { from: './devrig/ui-assets', to: 'ui-assets' },
-    { from: '../res', to: 'res' },
-  ];
-  if (isCompBundleDir) {
-    // if this is a directory, copy its assets to devrig too.
-    // currently 'images' is the only subpath supported for composition assets.
-    const baseDir = path.dirname(compositionImportPath);
-    const imagesDir = path.resolve(baseDir, 'images');
-    if (fs.existsSync(imagesDir)) {
-      console.log('will copy composition asset images: ', imagesDir);
-      dirsToCopy.push({ from: imagesDir, to: 'composition-assets/images' });
-    }
-  }
-
-  let isDev = true;
-
-  const compFilenameBase = compId.replace(/:/g, '_');
-
-  const useCompFilename = env.use_comp_filename;
-  if (useCompFilename) {
-    console.log('Exporting with comp filename, will use production mode');
-    isDev = false;
-  }
-
+function getBaseConfig() {
   return {
-    mode: isDev ? 'development' : 'production',
-    entry: {
-      devrig: './lib-browser/vcs-browser.js', //path.resolve(__dirname, '', 'vcs-browser.js'),
-    },
-    target: 'web',
-    output: {
-      library: {
-        name: `VCSComposition_${compFilenameBase}`,
-        type: 'window',
-      },
-      filename: useCompFilename
-        ? `${compFilenameBase}.bundle.js`
-        : '[name].bundle.js',
-      path: path.resolve('build'),
-      clean: true,
-    },
-    devServer: {
-      port: 8083,
-      static: {
-        directory: './build',
-      },
-    },
-    devtool: isDev ? 'cheap-source-map' : false,
     module: {
       rules: [
         {
@@ -139,6 +76,124 @@ export default function wwwClientConfig(env) {
         },
       ],
     },
+    resolve: {
+      alias: {
+        '#vcs': path.resolve('./src'),
+        '#vcs-react': path.resolve('./src/react'),
+        react: path.resolve('./node_modules/react'),
+      },
+      fallback: {
+        // following fallbacks are needed by textkit (from react-pdf)
+        process: require.resolve('process/browser'),
+        zlib: require.resolve('browserify-zlib'),
+        stream: require.resolve('stream-browserify'),
+        util: require.resolve('util'),
+        buffer: require.resolve('buffer'),
+        asset: require.resolve('assert'),
+      },
+    },
+  };
+}
+
+function getConfig_moduleWeb(env) {
+  let isDev = true;
+
+  const compFilenameBase = g_compId.replace(/:/g, '_');
+
+  return {
+    ...getBaseConfig(),
+    mode: 'development',
+    entry: './lib-browser/vcs-browser.js',
+    output: {
+      filename: `${compFilenameBase}.bundle.js`,
+      path: path.resolve('dist'),
+      library: {
+        type: 'commonjs2', //'module',
+      },
+    },
+    /*experiments: {
+      outputModule: true,
+    },
+    externalsType: 'module',
+    externals: {
+      react: {
+        commonjs: 'react',
+        commonjs2: 'react',
+        amd: 'react',
+        root: 'React',
+        module: 'react',
+      },
+    },*/
+    plugins: [
+      breakOnWarningPlugin,
+      moduleReplacementPlugin,
+
+      // following buffer plugin is needed by textkit (from react-pdf)
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+        process: 'process/browser',
+      }),
+    ],
+    devtool: isDev ? 'cheap-source-map' : false,
+  };
+}
+
+function getConfig_devRig(env) {
+  const isCompBundleDir =
+    path.basename(g_compositionImportPath).toLowerCase().indexOf('index.js') ===
+    0;
+
+  const dirsToCopy = [
+    { from: './devrig/example-assets', to: 'example-assets' },
+    { from: './devrig/ui-assets', to: 'ui-assets' },
+    { from: '../res', to: 'res' },
+  ];
+  if (isCompBundleDir) {
+    // if this is a directory, copy its assets to devrig too.
+    // currently 'images' is the only subpath supported for composition assets.
+    const baseDir = path.dirname(g_compositionImportPath);
+    const imagesDir = path.resolve(baseDir, 'images');
+    if (fs.existsSync(imagesDir)) {
+      console.log('will copy composition asset images: ', imagesDir);
+      dirsToCopy.push({ from: imagesDir, to: 'composition-assets/images' });
+    }
+  }
+
+  let isDev = true;
+
+  const compFilenameBase = g_compId.replace(/:/g, '_');
+
+  const useCompFilename = env.use_comp_filename;
+  if (useCompFilename) {
+    console.log('Exporting with comp filename, will use production mode');
+    isDev = false;
+  }
+
+  return {
+    ...getBaseConfig(),
+    mode: isDev ? 'development' : 'production',
+    entry: {
+      devrig: './lib-browser/vcs-browser.js', //path.resolve(__dirname, '', 'vcs-browser.js'),
+    },
+    target: 'web',
+    output: {
+      library: {
+        name: `VCSComposition_${compFilenameBase}`,
+        type: 'window',
+      },
+      filename: useCompFilename
+        ? `${compFilenameBase}.bundle.js`
+        : '[name].bundle.js',
+      path: path.resolve('build'),
+      clean: true,
+    },
+    devServer: {
+      port: 8083,
+      static: {
+        directory: './build',
+      },
+    },
+    devtool: isDev ? 'cheap-source-map' : false,
     plugins: [
       breakOnWarningPlugin,
       moduleReplacementPlugin,
@@ -168,21 +223,31 @@ export default function wwwClientConfig(env) {
     optimization: {
       minimize: !isDev,
     },
-    resolve: {
-      alias: {
-        '#vcs': path.resolve('./src'),
-        '#vcs-react': path.resolve('./src/react'),
-        react: path.resolve('./node_modules/react'),
-      },
-      fallback: {
-        // following fallbacks are needed by textkit (from react-pdf)
-        process: require.resolve('process/browser'),
-        zlib: require.resolve('browserify-zlib'),
-        stream: require.resolve('stream-browserify'),
-        util: require.resolve('util'),
-        buffer: require.resolve('buffer'),
-        asset: require.resolve('assert'),
-      },
-    },
   };
+}
+
+export default function wwwClientConfig(env) {
+  const compId = env.compid;
+  if (!compId || compId.length < 1) {
+    console.error(
+      '** Must provide VCS composition id (use webpack CLI arg --env compid={id})'
+    );
+    process.exit(2);
+  }
+  g_compId = compId;
+
+  if (!(g_compositionImportPath = getCompPathFromId(compId, 'browser'))) {
+    process.exit(3);
+  }
+
+  if (env.target === 'devrig') {
+    return getConfig_devRig(env);
+  } else if (env.target === 'module_web') {
+    return getConfig_moduleWeb(env);
+  } else {
+    console.error(
+      "** Must specify target using env.target (either 'devrig' or 'module_web')"
+    );
+    process.exit(4);
+  }
 }
