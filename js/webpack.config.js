b432805c4f05b9bc28f47606c607567d03096342
Pauli <pauli@lacquer.fi>
2023-09-19 10:34:45 +0300

    Remove unnecessary Babel plugins from VCS dev setup

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -79,10 +79,7 @@ function getBaseConfig() {
                   ],
                   '@babel/preset-react',
                 ],
-                plugins: [
-                  '@babel/plugin-proposal-class-properties',
-                  '@babel/plugin-transform-runtime',
-                ],
+                plugins: [],
               },
             },
           ],
